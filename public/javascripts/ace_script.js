4d9a6052de21cc578b86b68434d61d70fa8cc275
Alina Urbina <alin2525@gmail.com>
2018-05-18 00:02:34 -0600

    Editor: Last changes

--- public/javascripts/ace_script.js
+++ public/javascripts/ace_script.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
 	var editor = ace.edit("editor");
 	editor.setTheme("ace/theme/monokai");
 	editor.session.setMode("ace/mode/javascript");
+
+	var editor_3d = ace.edit("intermediate-editor");
+	editor_3d.setTheme("ace/theme/monokai");
+	editor_3d.session.setMode("ace/mode/javascript");
 });
