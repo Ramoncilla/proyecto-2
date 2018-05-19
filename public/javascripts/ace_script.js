73e271c3c3e27d8b21a5a69c8fe95040cce72b82
Alina Urbina <alin2525@gmail.com>
2018-05-19 16:47:15 -0600

    Compile from editor

--- public/javascripts/ace_script.js
+++ public/javascripts/ace_script.js
@@ -14,11 +14,68 @@ $(document).ready(function(){
 		alert('Message recieved: ' + data.message);
 	});
 
+	document.getElementById("openFile").addEventListener('change', function(){
+        var fr = new FileReader();
+        fr.onload=function(){
+          editor.setValue(this.result);
+      
+        }
+        fr.readAsText(this.files[0]);
+      
+      });
+
 	$('.build-link').on('click', function(e){
 		e.preventDefault();
-		console.log('Building');
-		socket.emit('send.message', {
-			message : "Hi"
-		})
+		var code = editor.getValue();
+		$.ajax(
+	      {
+	        type:"POST",
+	        url:" http://localhost:3000/parser",
+	        data:{string_file: code},
+	        success: function(data){
+	          console.dir(data);
+	        },
+	        error: function(data){
+	          console.dir(data);
+	        }
+	  
+	      }
+    	);
+		
+	});
+	$('.debug-link').on('click', function(e){
+		e.preventDefault();
+		
+		
+	});
+	$('.resume-link').on('click', function(e){
+		e.preventDefault();
+		
+		
+	});
+	$('.next-link').on('click', function(e){
+		e.preventDefault();
+		
+		
+	});
+	$('.skip-link').on('click', function(e){
+		e.preventDefault();
+		
+		
+	});
+	$('.automatic-link').on('click', function(e){
+		e.preventDefault();
+		
+		
+	});
+	$('.export-link').on('click', function(e){
+		e.preventDefault();
+		
+		
+	});
+	$('.import-link').on('click', function(e){
+		e.preventDefault();
+		
+		$('#openFile').click();
 	});
 });
